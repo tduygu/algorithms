5e5ea61cabade6b6a0898b395f53e8583f16d6ef
Duygu Tuncay <duygutuncay@hotmail.com>
2022-11-28 16:47:03 +0300

    ADS-4 calculating geometric mean

--- basicOperations/averaging.py
+++ basicOperations/averaging.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Average:
     def arithmeticMean(self, list1):
         total = 0
@@ -8,7 +11,20 @@ class Average:
         return avg
 
     def geometricMean(self, list1):
-        pass
+        multply = 1
+        for a in list1:
+            multply *= a
+
+        avg = multply ** (1/len(list1))
+        return avg
+
+    # def logofgeometricmean(self, list1):
+    #     total = 0
+    #     for a in list1:
+    #         total += math.log(a)
+    #
+    #     avg = total / len(list1)
+    #     return avg
 
 
 lst = []
@@ -22,4 +38,9 @@ print(lst)
 
 art = Average()
 mean_arithmetic = art.arithmeticMean(lst)
-print(f"Arithmetic mean of the given list is {mean_arithmetic}")
+mean_geometric = art.geometricMean(lst)
+# mean_geometric2 = art.logOfGeometricMean(lst)
+print(f"Arithmetic mean of the given list is {mean_arithmetic} \n"
+      f"Geometric mean of the given list is {mean_geometric}")
+
+      # f"log of geometric mean is: {mean_geometric2}"
