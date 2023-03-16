5b32b68f0b80101297573cf811349505d3f3adc3
Duygu Tuncay <duygutuncay@hotmail.com>
2023-03-16 16:13:32 +0300

    ADS-6 basic binary search

--- basicOperations/search.py
+++ basicOperations/search.py
@@ -6,9 +6,29 @@ class SearchOperations:
                 return indnum
         return -1
 
+    def binarySearch(self, searchedOne, orderedList):
+        left = 0
+        right = len(orderedList) - 1
+
+        while left <= right:
+            middle = int((left + right) / 2)
+            print(middle)
+            if searchedOne == orderedList[middle]:
+                return middle
+            elif searchedOne > orderedList[middle]:
+                left = middle + 1
+            else:
+                right = middle -1
+        return -1
+
 
 to_be_searched = (int)(input("Provide the integer number to be searched for:"))
 s = SearchOperations()
-found_index = s.sequentialSearch(to_be_searched, [1, 5, 4, 8, 2, 90, 67, 3])
+# sequential search
+# found_index = s.sequentialSearch(to_be_searched, [1, 5, 4, 8, 2, 90, 67, 3])
+
+# binary search
+found_index = s.binarySearch(to_be_searched, [1,2,4,7,70,79,82,99,500])
+
 str_result = f"The searched number {to_be_searched} is at index {found_index}" if found_index > -1 else f"The searched number does not exist in the list."
 print(str_result)
