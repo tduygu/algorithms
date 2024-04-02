d1d076c5ae6147197fcf7ea558e152d2e461154d
Duygu Tuncay <duygutuncay@hotmail.com>
2024-04-02 14:49:41 +0300

    TRAIN-7 string comparisons and searching

---
+++ pcap/strings/string-comparisons.py
@@ -0,0 +1,20 @@
+print('Python' == 'Python')
+print('Python' == 'python')
+print('Python' != 'python')
+
+print(
+'I love Python' < 'I love python'
+)
+
+print('Pythonist' > 'Python')
+
+print('Pythonist' > 'Z')
+
+print("Python comapres according to the first character's encodings's code number")
+
+print(10 == '10')
+
+print('20'<'8')
+
+print(10 > '10')
+
