285582474646e4934ca63b021c4e8eae21e1aa86
Duygu Tuncay <duygutuncay@hotmail.com>
2024-07-19 11:36:57 +0300

    Examples of lists, tuples, zip function

--- general/exercises/list-ops.py
+++ general/exercises/list-ops.py
@@ -1,35 +1,36 @@
+"""Strings are immutable"""
 # filename = "1.file1.txt"
 # filename = "1- file1.txt"
 # print(filename)
 # print(filename[1])
-# filename[1] = "*" # strings are immutable
-# #filename = filename.replace('-','*')
-# #print(filename)
+# #filename[1] = "*" # strings are immutable
+# filename = filename.replace('-','*')
+# print(filename)
 #
 # filenames = ['1.st file', '2nd. file', '3rd file']
 # filenames[0] = "-"
-#
+# #
 # print(filenames)
 #
 # # tuple is immutable version of a list
 
 ################################################################
+"""Enumerate a list"""
 # a = enumerate(['x','y','z'])
-# print(a)
-#
-# #print(list(a))
+# # print(a)
+# # print(list(a))
 #
 # b = list(a)
-#
 # print(b)
 # b.pop(1)
 # print(b)
 
 ################################################################
-# a = [1, 2, 3]
-# b = [10, 20 ,30]
-# x = zip(a, b)
-# print(list(x))
+"""zip example"""
+a = [1, 2, 3]
+b = [10, 20 ,30]
+x = zip(a, b)
+print(list(x))
 
 ################################################################
 # contents = ["All carrots are to be sliced longitudinally",
@@ -47,9 +48,9 @@
 #        "my own"
 # print(txt)
 ################################################################
-filenames = ['a.txt', 'b.txt', 'c.txt']
-for f in filenames:
-       file = open(f"files/{f}", 'r')
-       print(file.read())
-       file.close()
+# filenames = ['a.txt', 'b.txt', 'c.txt']
+# for f in filenames:
+#        file = open(f"files/{f}", 'r')
+#        print(file.read())
+#        file.close()
 
