09fb20fc2fc6d1055d4767a5e652192d8ebc7b30
Duygu Tuncay <duygutuncay@hotmail.com>
2024-07-22 16:09:23 +0300

    listing instagram followers and followings

--- general/instagram/show_instagram_followers.py
+++ general/instagram/show_instagram_followers.py
@@ -1,3 +1,12 @@
+"""This code lists :
+ * the instagram followers of your account
+ * the people who you follow - followings
+ * followers that you don't follow
+ * not followers that you follow
+
+ You must change the files under insta_files directory
+ with your followers and followings files which must be in instagram's json file format
+"""
 import json
 
 def get_json_content(filename):
@@ -17,7 +26,7 @@ def get_names_followers(stream_var):
     for s in y:
         f = s['string_list_data'][0]['value']
         lst_follower.append(f)
-        #print(f)
+        print(f)
     return lst_follower
 
 def get_names_following(stream_var):
@@ -27,28 +36,28 @@ def get_names_following(stream_var):
     for s in z['relationships_following']:
         f = s['string_list_data'][0]['value']
         lst_following.append(f)
-        #print(f)
+        print(f)
     return lst_following
 
 print("Followers",end='\n')
-follower_var = get_json_content('insta/followers_1.json')
+follower_var = get_json_content('../insta_files/followers_1.json')
 follower_lst = get_names_followers(follower_var)
 print(f"Number of follower= {len(follower_lst)}")
 
-print("Following",end='\n')
-following_var = get_json_content('insta/following.json')
+print("Followings",end='\n')
+following_var = get_json_content('../insta_files/following.json')
 following_lst = get_names_following(following_var)
-print(f"Number of following = {len(following_lst)}")
+print(f"Number of followings = {len(following_lst)}")
 
-# not_followers = []
-# for ff in following_lst:
-#     if ff not in follower_lst:
-#         not_followers.append(ff)
-#
-# print("NOT FOLLOWERS")
-# print(f"Takipçi olmayan takip edilen sayısı: {len(not_followers)}")
-# for nf in not_followers:
-#     print(nf)
+not_followers = []
+for ff in following_lst:
+    if ff not in follower_lst:
+        not_followers.append(ff)
+
+print("NOT FOLLOWERS THAT I FOLLOW")
+print(f"Takipçi olmayan takip edilen sayısı: {len(not_followers)}")
+for nf in not_followers:
+    print(nf)
 
 
 print("FOLLOWERS THAT I DON'T FOLLOW")
