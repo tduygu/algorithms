ddbaaffa77a9b893dff569169c1e646001b4fc8d
Duygu Tuncay <duygutuncay@hotmail.com>
2024-07-23 19:35:04 +0300

    rearranging files

--- general/json_sample.py
+++ general/json_sample.py
@@ -1,6 +1,6 @@
 import json
 
-with open('backup_config.json') as fh:
+with open('../backup_config.json') as fh:
     conf = json.load(fh)
 
 print(conf)
@@ -8,6 +8,6 @@ print(type(conf))
 print(conf["read_db"])
 
 conf["new_key"] = 5.56789
-with open('backup_config.json', 'w') as fh:
+with open('../backup_config.json', 'w') as fh:
     # json.dump(conf, fh)
     json.dump(conf, fh, indent=4, separators=(',', ': '))
