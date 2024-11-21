acdf819963d05b24c5211c227ea7ba625c87db3d
Duygu Tuncay <duygutuncay@hotmail.com>
2024-11-21 12:21:47 +0300

    ADS-39 constructing OCP by the enterprise pattern specification

--- designPatterns/work0002.py
+++ designPatterns/work0002.py
@@ -1,6 +1,9 @@
 #SOLID - OCP - Open Close Principle
 from enum import Enum
 
+from aptdaemon.logger import GREEN
+from reportlab.platypus.paraparser import sizeDelta
+
 
 class Color(Enum):
     RED = 1
@@ -37,4 +40,73 @@ class ProductFilter:
             if p.color == color and p.size == size:
                 yield p
 
-    
+    # Enterprise pattern - Specification
+
+class Specification:
+    def is_satisfied(self, item):
+        pass
+
+class AndSpecification(Specification):
+    def __init__(self, *args):
+        self.args = args
+
+    def is_satisfied(self, item):
+        return all(map(
+            lambda spec: spec.is_satisfied(item), self.args
+        ))
+
+class Filter:
+    def filter(self, items, spec):
+        pass
+
+class ColorSpecification(Specification):
+    def __init__(self, color):
+        self.color = color
+
+    def is_satisfied(self, item):
+        return item.color == self.color
+
+class SizeSpecification(Specification):
+    def __init__(self, size):
+        self.size = size
+
+    def is_satisfied(self, item):
+        return item.size == self.size
+
+class BetterFilter(Filter):
+    def filter(self, items, spec):
+        for item in items:
+            if spec.is_satisfied(item):
+                yield item
+
+
+if __name__ == '__main__':
+    apple = Product('Apple', Color.GREEN, Size.SMALL)
+    tree = Product('Tree', Color.GREEN, Size.LARGE)
+    house = Product('House', Color.BLUE, Size.LARGE)
+
+    products = [apple, tree, house]
+    pf = ProductFilter()
+    print('Green products (old):')
+    for p in pf.filter_by_color(products, Color.GREEN):
+        print(f' {p.name} is green')
+
+    bf = BetterFilter()
+    print('Green products (new):')
+    for p in bf.filter(products, ColorSpecification(Color.GREEN)):
+        print(f' {p.name} is Green')
+
+    large = SizeSpecification(Size.LARGE)
+    for p in bf.filter(products, SizeSpecification(large)):
+        print(f' {p.name} is large')
+
+    print("Large blue items:")
+    large_blue = AndSpecification(large, ColorSpecification(Color.BLUE))
+    for p in bf.filter(products, large_blue):
+        print(f' {p.name} is large and blue')
+
+
+
+
+
+
